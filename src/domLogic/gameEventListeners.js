b0243ee44c2239cf2aad8c6f965aed524e8ed546
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-10-17 22:15:20 -0700

    Move event listener logic to gameEventListeners

--- src/domLogic/gameEventListeners.js
+++ src/domLogic/gameEventListeners.js
@@ -1,15 +1,40 @@
-export function addBoardListener(domBoard, handler) {
-  domBoard.addEventListener("click", handler);
+const cpuDomBoard = document.querySelector(".cpu-board");
+const nextBtn = document.getElementById("next");
+
+export function disablePlayerTurn(handler) {
+  cpuDomBoard.removeEventListener("click", handler);
 }
 
-export function removeBoardListener(domBoard, handler) {
-  domBoard.removeEventListener("click", handler);
+export function activatePlayerTurn(
+  domBoard,
+  playerShotHandler,
+  renderHits,
+  renderMisses,
+) {
+  function shootingLogic(e) {
+    const domCoordinate = e.target;
+
+    playerShotHandler(domCoordinate, domBoard);
+
+    renderHits(domBoard, cpuDomBoard);
+    renderMisses(domBoard, cpuDomBoard);
+
+    disablePlayerTurn(cpuDomBoard, shootingLogic); // stops after one shot
+
+    nextBtn.setAttribute("aria-disabled", "false");
+  }
+
+  domBoard.addEventListener("click", shootingLogic);
 }
 
-export function addNextBtnListener(nextBtn, handler) {
-  nextBtn.addEventListener("click", handler);
-}
-
-export function removeNextBtnListener(nextBtn, handler) {
+export function disableCpuTurn(handler) {
   nextBtn.removeEventListener("click", handler);
+}
+
+export function activateCpuTurn() {
+  function cpuShootingLogic() {
+    disableCpuTurn(nextBtn, cpuShootingLogic);
+  }
+
+  nextBtn.addEventListener("click", cpuShootingLogic);
 }
