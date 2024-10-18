7acc8d2a9125b81166f24ba986c28ef946c295e8
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-10-17 22:12:21 -0700

    Modularize event listeners for readability

--- src/domLogic/domGameController.js
+++ src/domLogic/domGameController.js
@@ -1,10 +1,5 @@
-import { renderBoard, renderMisses } from "./boardRender";
-import {
-  addBoardListener,
-  addNextBtnListener,
-  removeBoardListener,
-  removeNextBtnListener,
-} from "./gameEventListeners";
+import { renderBoard, renderHits, renderMisses } from "./boardRender";
+import { activateCpuTurn, activatePlayerTurn } from "./gameEventListeners";
 
 const cpuDomBoard = document.querySelector(".cpu-board");
 const playerDomBoard = document.querySelector(".player-board");
@@ -25,8 +20,6 @@ function playerShotHandler(coordinate, cpuBoard) {
     )
   ) {
     if (hit.hitLocation) {
-      coordinate.classList.add("hit");
-
       gameInfo.textContent = "Hit!";
     } else if (!hit.hitLocation) {
       gameInfo.textContent = "Miss.";
@@ -40,32 +33,16 @@ export default function startGame(player, cpu) {
   const playerBoard = player.board;
   const cpuBoard = cpu.board;
 
-  function shootingLogic(e) {
-    const domCoordinate = e.target;
-
-    playerShotHandler(domCoordinate, cpuBoard);
-    renderMisses(cpuBoard, cpuDomBoard);
-
-    removeBoardListener(cpuDomBoard, shootingLogic); // stops after one shot
-
-    nextBtn.setAttribute("aria-disabled", "false");
-  }
-
-  function cpuShootingLogic() {
-    removeNextBtnListener(nextBtn, cpuShootingLogic);
-  }
-
   nextBtn.setAttribute("aria-disabled", "true");
+
+  renderHits(cpuBoard, cpuDomBoard);
+  renderMisses(cpuBoard, cpuDomBoard);
 
   gameInfo.textContent = "Take your shot.";
 
-  addBoardListener(cpuDomBoard, shootingLogic);
+  activatePlayerTurn(cpuBoard, playerShotHandler, renderHits, renderMisses);
 
   if (cpuDomBoard.getAttribute("aria-disabled") === "true") {
-    addNextBtnListener(nextBtn, cpuShootingLogic);
+    activateCpuTurn();
   }
-
-  // addNextListener will trigger a CPU shot
 }
-
-// will need to create new player and cpu objects!!
