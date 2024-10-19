d7eed8f5f94cbbb1787505858fbdd277f20b7742
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-10-19 15:20:45 -0700

    Fnsh cpuTrn/plrTrn,ad alwTrn funcs for readability

--- src/domLogic/domGameController.js
+++ src/domLogic/domGameController.js
@@ -1,13 +1,30 @@
 /* eslint-disable no-param-reassign */
 
-import { renderBoard, renderHits, renderMisses } from "./boardRender";
+import {
+  hideDomBoard,
+  renderBoard,
+  renderHits,
+  renderMisses,
+  showDomBoard,
+} from "./boardRender";
 
 const playerDomBoard = document.querySelector(".player-board");
 const cpuDomBoard = document.querySelector(".cpu-board");
 
+const playerName = document.querySelector("p:nth-child(2)");
+const cpuName = document.querySelector("p:nth-child(4)");
+
 const nextBtn = document.getElementById("next");
 
 const gameInfo = document.querySelector(".game-info");
+
+function allowPlayerTurn() {
+  cpuDomBoard.setAttribute("aria-disabled", "false");
+}
+
+function allowCpuTurn() {
+  cpuDomBoard.setAttribute("aria-disabled", "true");
+}
 
 function announceHitStatus(hit) {
   if (hit) {
@@ -17,14 +34,15 @@ function announceHitStatus(hit) {
   }
 }
 
-export function playerTurn(cpuBoard) {
-  renderHits(cpuBoard, cpuDomBoard);
-  renderMisses(cpuBoard, cpuDomBoard);
+function cpuShotHandler(cpu, playerBoard) {
+  const hit = cpu.randomShot(playerBoard);
 
-  gameInfo.textContent = "Take your shot.";
+  renderBoard(playerBoard, playerDomBoard);
 
-  cpuDomBoard.setAttribute("aria-disabled", "false");
-  nextBtn.disabled = true;
+  announceHitStatus(hit);
+
+  allowPlayerTurn();
+  nextBtn.disabled = false;
 }
 
 export function activatePlayerShooting(cpuBoard) {
@@ -40,12 +58,38 @@ export function activatePlayerShooting(cpuBoard) {
 
       announceHitStatus(hit);
 
-      cpuDomBoard.setAttribute("aria-disabled", "true");
+      allowCpuTurn();
       nextBtn.disabled = false;
     }
   });
 }
 
-function cpuShotHandler(cpu, playerBoard) {
-  const hit = cpu.randomShot(playerBoard);
+export function playerTurn(cpuBoard) {
+  showDomBoard(cpuName, cpuDomBoard);
+  hideDomBoard(playerName, playerDomBoard);
+
+  renderHits(cpuBoard, cpuDomBoard);
+  renderMisses(cpuBoard, cpuDomBoard);
+
+  gameInfo.textContent = "Take your shot.";
+
+  allowPlayerTurn();
+  nextBtn.disabled = true;
+}
+
+export function cpuTurn(cpu, playerBoard) {
+  showDomBoard(playerName, playerDomBoard);
+  hideDomBoard(cpuName, cpuDomBoard);
+
+  renderBoard(playerBoard, playerDomBoard);
+
+  nextBtn.disabled = true;
+
+  gameInfo.textContent = "CPU is taking a shot...";
+
+  const shot = () => {
+    cpuShotHandler(cpu, playerBoard);
+  };
+
+  setTimeout(shot, 1500);
 }
