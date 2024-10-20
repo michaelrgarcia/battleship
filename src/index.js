0d2099879f70e12fdeac3dbdd4d6b5aae7296358
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-10-20 01:05:23 -0700

    Crete plyAgin btn +closre 4 evnt listnr gme access

--- src/index.js
+++ src/index.js
@@ -1,83 +1,95 @@
 import {
+  clearDomBoard,
   hideDomBoard,
   renderBoard,
   showDomBoard,
 } from "./domLogic/boardRender";
+
 import {
   activatePlayerShooting,
   cpuTurn,
   playerTurn,
 } from "./domLogic/domGameController";
+
 import { RealPlayer, CpuPlayer } from "./gameLogic/playerLogic";
 
-const player = RealPlayer();
-const playerDomBoard = document.querySelector(".player-board");
+function newGame(player, cpu, playerDomBoard) {
+  cpu.randomizeBoard();
 
-const shuffle = document.getElementById("shuffle");
-const go = document.getElementById("go");
-const next = document.getElementById("next");
+  player.randomizeBoard();
+  renderBoard(player.board, playerDomBoard);
+}
 
-player.board.placeShip([
-  [2, 2],
-  [2, 3],
-  [2, 4],
-]);
+(function game() {
+  const playerDomBoard = document.querySelector(".player-board");
+  const cpuDomBoard = document.querySelector(".cpu-board");
 
-player.board.placeShip([
-  [2, 7],
-  [3, 7],
-  [4, 7],
-  [5, 7],
-]);
-
-renderBoard(player.board, playerDomBoard);
-
-const cpu = CpuPlayer();
-const cpuDomBoard = document.querySelector(".cpu-board");
-
-cpu.board.placeShip([
-  [9, 3],
-  [9, 4],
-  [9, 5],
-]);
-
-cpu.board.placeShip([
-  [3, 10],
-  [4, 10],
-  [5, 10],
-]);
-
-shuffle.addEventListener("click", () => {
-  // shuffling code. will be located in player logic
-});
-
-activatePlayerShooting(cpu.board, player.board);
-
-go.addEventListener("click", () => {
   const playerName = document.querySelector("p:nth-child(2)");
   const cpuName = document.querySelector("p:nth-child(4)");
 
-  showDomBoard(cpuName, cpuDomBoard);
-  hideDomBoard(playerName, playerDomBoard);
+  const gameInfo = document.querySelector(".game-info");
 
-  shuffle.style.display = "none";
-  go.style.display = "none";
+  const shuffle = document.getElementById("shuffle");
+  const go = document.getElementById("go");
+  const next = document.getElementById("next");
+  const playAgain = document.getElementById("play-again");
 
-  next.style.display = "block";
+  let player = RealPlayer();
+  let cpu = CpuPlayer();
 
-  playerTurn(cpu.board);
-});
+  newGame(player, cpu, playerDomBoard, gameInfo);
 
-next.addEventListener("click", () => {
-  function playerTurnAllowed() {
-    const allowed = cpuDomBoard.ariaDisabled === "false";
+  shuffle.addEventListener("click", () => {
+    player.board.clearBoard();
+    clearDomBoard(playerDomBoard);
 
-    return allowed;
-  }
+    player.randomizeBoard();
+    renderBoard(player.board, playerDomBoard);
+  });
 
-  if (playerTurnAllowed()) {
+  go.addEventListener("click", () => {
+    showDomBoard(cpuName, cpuDomBoard);
+    hideDomBoard(playerName, playerDomBoard);
+
+    shuffle.style.display = "none";
+    go.style.display = "none";
+
+    next.style.display = "block";
+
     playerTurn(cpu.board);
-  } else {
-    cpuTurn(cpu, player.board);
-  }
-});
+  });
+
+  activatePlayerShooting(cpu.board, player.board);
+
+  next.addEventListener("click", () => {
+    function playerTurnAllowed() {
+      const allowed = cpuDomBoard.ariaDisabled === "false";
+
+      return allowed;
+    }
+
+    if (playerTurnAllowed()) {
+      playerTurn(cpu.board);
+    } else {
+      cpuTurn(cpu, player.board);
+    }
+  });
+
+  playAgain.addEventListener("click", () => {
+    hideDomBoard(playerName, playerDomBoard);
+    hideDomBoard(cpuName, cpuDomBoard);
+
+    clearDomBoard(playerDomBoard);
+    clearDomBoard(cpuDomBoard);
+
+    player = RealPlayer();
+    cpu = CpuPlayer();
+
+    newGame(player, cpu, playerDomBoard);
+
+    showDomBoard(playerName, playerDomBoard);
+
+    gameInfo.textContent =
+      "Click the shuffle button to place the ships to your liking.";
+  });
+})();
